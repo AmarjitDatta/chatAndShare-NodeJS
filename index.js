8d92c6391dd9afe585c4429c86e92805265b0946
Avra Ratan <avra.ratan@gmail.com>
2015-03-02 23:28:22 +0600

    Updated code with proper naming convention.

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var socket = require('socket.io')(http);
+var people = {};
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-	socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+socket.on('connection', function(client){
+	client.on('chat message', function(msg){
+    socket.emit('chat message', msg);
   });
 });
 
