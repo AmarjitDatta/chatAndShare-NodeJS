1e512c540428a35ca5cc2b9029b33468cc60abb5
Avra Ratan <avra.ratan@gmail.com>
2015-03-03 00:08:18 +0600

    Added chat room contact options in code.

--- index.js
+++ index.js
@@ -4,15 +4,25 @@ var socket = require('socket.io')(http);
 var people = {};
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
 socket.on('connection', function(client){
+	client.on('join', function(name){
+		people[client.id] = name;
+		socket.emit('join', name);
+		socket.emit("update-people", people);
+	});
 	client.on('chat message', function(msg){
-    socket.emit('chat message', msg);
-  });
+		socket.emit('chat message', msg);
+	});
+	client.on("disconnect", function(){
+        socket.emit("chat message", people[client.id] + " has left the server.");
+        delete people[client.id];
+        socket.emit("update-people", people);
+    });
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+	console.log('listening on *:3000');
 });
